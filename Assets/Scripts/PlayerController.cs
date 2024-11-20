5047577d9e8bab83a1f83525a7213c04eb3ea927
prithibi1103 <prithibid@nycstudents.net>
2024-11-20 09:13:04 -0500

    i changed the design of the map and i gave the character movement.

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerController : MonoBehaviour
+{ 
+
+  public InputAction moveAction;
+  // Start is called before the first frame update
+  void Start()
+   
+  {
+   moveAction.Enable(); 
+  }
+   
+  
+  // Update is called once per frame
+  void Update()
+  {
+    
+    Vector2 move = moveAction.ReadValue<Vector2>();
+
+
+    Debug.Log(move);
+
+
+    Vector2 position = (Vector2)transform.position + move * 0.1f * Time.deltaTime;
+    transform.position = position;
+
+  }
+
+}
+
+
